2ec0851719f7cef876d4cfe6dac7e9bb7fe62ebe
PrettyRut <antwi4m@yahoo.co.uk>
2023-04-24 06:49:50 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,6 +5,6 @@
 * Return: 1 (not success)
 */
 int main(void)
-char quo[] = "and that piece of art is useful" - Dora Korpar, 2015-10-19\n";
+char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(1,quo,59);
 return (1);
