f69632557c7427c5e11b54d25d6a0c756c55e583
PrettyRut <antwi4m@yahoo.co.uk>
2023-05-05 06:47:17 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,22 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 /**
-* leet - encode into 1337speak
-* @n: input value
-* Return: n value
+* rot13 - encoder rot13
+* @s: pointer to string params
+*
+* Return: *s
 */
-char *leet(char *n)
+char *rot13(char *s)
 {
-int i, j;
-char s1[] = "aAeEoOtTlL";
-char s2[] = "4433007711";
-for (i = 0; n[i] != '\0'; i++)
+int i;
+int j;
+char data1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+for (i = 0; s[i] != '\0'; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = 0; j < 52; j++)
 {
-if (n[i] == s1[j])
+if (s[i] == data1[j])
 {
-n[i] = s2[j];			}
+s[i] = datarot[j];
+break;
 }
 }
-return (n);
+}
+return (s);
 }
